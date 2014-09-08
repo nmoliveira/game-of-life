71e180c965ff5dd8f89b88a4e84d0861d5e55ef3
Nuno Oliveira <nmoliveira@gmail.com>
2014-09-08 23:41:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+HTML5 Conway's Game of Life
+
+
+The "game" is a zero-player game, meaning that its evolution is determined by its initial configuration and does not require any other input. You can setup an initial configuration by clicking on the cells or  selecting one of the predefined patterns. After configuring the initial pattern, just hit start and see the game evolving. You can set the time between each iteration, the lower the interval the faster it evolves. 
+
+There are only 4 rules that determines how the game evolves:
+
+1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.</li>
+2. Any live cell with two or three live neighbours lives on to the next generation.</li>
+3. Any live cell with more than three live neighbours dies, as if by overcrowding.</li>
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+
+<a href="http://en.wikipedia.org/wiki/Conway's_Game_of_Life" target="_blank">Game of Life on Wikipedia</a>
+
+[Demo](http://nunooliveira.me/games/game-of-life)
---
+++ spec/GameSpec.js
@@ -0,0 +1,224 @@
+describe("Cells", function () {
+
+    it("Should be able to create cells", function () {
+        var cell = new GameOfLife.Cell(0, 0);
+        expect(cell).toBeTruthy();
+    })
+
+});
+
+describe("Lines", function () {
+
+    it("Should be able to create lines", function () {
+        var line = GameOfLife.Line(GameOfLife.Cell, 0, 1);
+        expect(line).toBeTruthy();
+    })
+
+});
+
+describe("Game of Life", function () {
+
+    var game;
+
+    beforeEach(function () {
+        game = new GameOfLife.Game(GameOfLife.Line, GameOfLife.Cell);
+    });
+
+    it("Should exist", function () {
+        expect(game).toBeDefined();
+    });
+
+    it("Should be able to define an initial board", function () {
+        game.createBoard(3, 3);
+
+        // number of lines should be 3
+        expect(game.board.lines.length).toEqual(3);
+
+        // number of cells per line should be 3
+        expect(game.board.lines[0].cells.length).toEqual(3);
+        expect(game.board.lines[1].cells.length).toEqual(3);
+        expect(game.board.lines[2].cells.length).toEqual(3);
+    });
+
+    it("Should be able to clear the board", function () {
+        game.createBoard(3, 3);
+        
+        // initial state
+        // [1,1,1]
+        // [1,1,1]
+        // [1,1,1]
+
+        game.board.lines[0].cells[0].active(true);
+        game.board.lines[0].cells[1].active(true);
+        game.board.lines[0].cells[2].active(true);
+        game.board.lines[1].cells[0].active(true);
+        game.board.lines[1].cells[1].active(true);
+        game.board.lines[1].cells[2].active(true);
+        game.board.lines[2].cells[0].active(true);
+        game.board.lines[2].cells[1].active(true);
+        game.board.lines[2].cells[2].active(true);
+
+        game.clearBoard();
+
+        // final state
+        // [0,0,0]
+        // [0,0,0]
+        // [0,0,0]
+
+        expect(game.board.lines[0].cells[0].active()).toEqual(false);
+        expect(game.board.lines[0].cells[1].active()).toEqual(false);
+        expect(game.board.lines[0].cells[2].active()).toEqual(false);
+        expect(game.board.lines[1].cells[0].active()).toEqual(false);
+        expect(game.board.lines[1].cells[1].active()).toEqual(false);
+        expect(game.board.lines[1].cells[2].active()).toEqual(false);
+        expect(game.board.lines[2].cells[0].active()).toEqual(false);
+        expect(game.board.lines[2].cells[1].active()).toEqual(false);
+        expect(game.board.lines[2].cells[2].active()).toEqual(false);
+    });
+
+    it("Should count the number of alive neighbours", function () {
+        game.createBoard(3, 3);
+
+        // initial state
+        // [0,0,0]
+        // [1,1,1]
+        // [0,0,0]
+
+        game.board.lines[1].cells[0].active(true);
+        game.board.lines[1].cells[1].active(true);
+        game.board.lines[1].cells[2].active(true);
+
+        expect(game.getLiveNeighbours(0, 0)).toEqual(2);
+        expect(game.getLiveNeighbours(0,1)).toEqual(3);
+        expect(game.getLiveNeighbours(0,2)).toEqual(2);
+        expect(game.getLiveNeighbours(1,0)).toEqual(1);
+        expect(game.getLiveNeighbours(1,1)).toEqual(2);
+        expect(game.getLiveNeighbours(1,2)).toEqual(1);
+        expect(game.getLiveNeighbours(2,0)).toEqual(2);
+        expect(game.getLiveNeighbours(2,1)).toEqual(3);
+        expect(game.getLiveNeighbours(2,2)).toEqual(2);
+
+    });
+
+    it("Should create Blinker pattern", function () {
+        
+        // The initial pattern for Blinker
+        // [0,0,0]
+        // [1,1,1]
+        // [0,0,0]
+        
+        game.createBoard(3, 3);
+        
+        game.board.lines[1].cells[0].active(true);
+        game.board.lines[1].cells[1].active(true);
+        game.board.lines[1].cells[2].active(true);
+
+        game.next();
+
+        // The next  state
+        // [0,1,0]
+        // [0,1,0]
+        // [0,1,0]
+        expect(game.board.lines[0].cells[0].active()).toEqual(false);
+        expect(game.board.lines[0].cells[1].active()).toEqual(true);
+        expect(game.board.lines[0].cells[2].active()).toEqual(false);
+        expect(game.board.lines[1].cells[0].active()).toEqual(false);
+        expect(game.board.lines[1].cells[1].active()).toEqual(true);
+        expect(game.board.lines[1].cells[2].active()).toEqual(false);
+        expect(game.board.lines[2].cells[0].active()).toEqual(false);
+        expect(game.board.lines[2].cells[1].active()).toEqual(true);
+        expect(game.board.lines[2].cells[2].active()).toEqual(false);
+
+    });
+
+    it("Should create Toad pattern", function () {
+
+        // The initial pattern for Toad
+        // [0,0,0,0]
+        // [0,1,1,1]
+        // [1,1,1,0]
+        // [0,0,0,0]
+        
+        game.createBoard(4, 4);
+        
+        game.board.lines[1].cells[1].active(true);
+        game.board.lines[1].cells[2].active(true);
+        game.board.lines[1].cells[3].active(true);
+        game.board.lines[2].cells[0].active(true);
+        game.board.lines[2].cells[1].active(true);
+        game.board.lines[2].cells[2].active(true);
+
+        game.next();
+
+        // The next  state
+        // [0,0,1,0]
+        // [1,0,0,1]
+        // [1,0,0,1]
+        // [0,1,0,0]
+
+        expect(game.board.lines[0].cells[0].active()).toEqual(false);
+        expect(game.board.lines[0].cells[1].active()).toEqual(false);
+        expect(game.board.lines[0].cells[2].active()).toEqual(true);
+        expect(game.board.lines[0].cells[3].active()).toEqual(false);
+        expect(game.board.lines[1].cells[0].active()).toEqual(true);
+        expect(game.board.lines[1].cells[1].active()).toEqual(false);
+        expect(game.board.lines[1].cells[2].active()).toEqual(false);
+        expect(game.board.lines[1].cells[3].active()).toEqual(true);
+        expect(game.board.lines[2].cells[0].active()).toEqual(true);
+        expect(game.board.lines[2].cells[1].active()).toEqual(false);
+        expect(game.board.lines[2].cells[2].active()).toEqual(false);
+        expect(game.board.lines[2].cells[3].active()).toEqual(true);
+        expect(game.board.lines[3].cells[0].active()).toEqual(false);
+        expect(game.board.lines[3].cells[1].active()).toEqual(true);
+        expect(game.board.lines[3].cells[2].active()).toEqual(false);
+        expect(game.board.lines[3].cells[3].active()).toEqual(false);
+
+    });
+
+    it("Should create Beacon pattern", function () {
+        
+        // initial state for Beacon
+        // [1,1,0,0]
+        // [1,1,0,0]
+        // [0,0,1,1]
+        // [0,0,1,1]
+
+        game.createBoard(4, 4);
+
+        game.board.lines[0].cells[0].active(true);
+        game.board.lines[0].cells[1].active(true);
+        game.board.lines[1].cells[0].active(true);
+        game.board.lines[1].cells[1].active(true);
+        game.board.lines[2].cells[2].active(true);
+        game.board.lines[2].cells[3].active(true);
+        game.board.lines[3].cells[2].active(true);
+        game.board.lines[3].cells[3].active(true);
+
+        game.next();
+
+        // final state
+        // [1,1,0,0]
+        // [1,0,0,0]
+        // [0,0,0,1]
+        // [0,0,1,1]
+
+        expect(game.board.lines[0].cells[0].active()).toEqual(true);
+        expect(game.board.lines[0].cells[1].active()).toEqual(true);
+        expect(game.board.lines[0].cells[2].active()).toEqual(false);
+        expect(game.board.lines[0].cells[3].active()).toEqual(false);
+        expect(game.board.lines[1].cells[0].active()).toEqual(true);
+        expect(game.board.lines[1].cells[1].active()).toEqual(false);
+        expect(game.board.lines[1].cells[2].active()).toEqual(false);
+        expect(game.board.lines[1].cells[3].active()).toEqual(false);
+        expect(game.board.lines[2].cells[0].active()).toEqual(false);
+        expect(game.board.lines[2].cells[1].active()).toEqual(false);
+        expect(game.board.lines[2].cells[2].active()).toEqual(false);
+        expect(game.board.lines[2].cells[3].active()).toEqual(true);
+        expect(game.board.lines[3].cells[0].active()).toEqual(false);
+        expect(game.board.lines[3].cells[1].active()).toEqual(false);
+        expect(game.board.lines[3].cells[2].active()).toEqual(true);
+        expect(game.board.lines[3].cells[3].active()).toEqual(true);
+
+    });
+
+});
